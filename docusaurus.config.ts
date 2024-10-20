11e7a2712351061f9641c103a6a1fc7f842ed039
Ramana Venkata <vramana@users.noreply.github.com>
2024-10-20 11:40:15 +0530

    Update production url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,15 +7,15 @@ const config: Config = {
   tagline: "An HTTP/1.1 client, written from scratch for Node.js.",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://undici-docs.vramana.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "nodejs", // Usually your GitHub org/user name.
+  projectName: "undici", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
